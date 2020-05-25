2c0f2e42579cb611cc0833920b6b2cd790b0e3d2
Fabien <fabienponcet.69@gmail.com>
2020-05-25 15:22:46 +0200

    Premier comit, installation et page d'accueil

---
+++ SymBNB/src/Controller/HomeController.php
@@ -0,0 +1,47 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+
+
+class HomeController extends AbstractController {
+
+
+    /**
+     * @Route("/hello/{prenom}/{age}", name = "hello")
+     * @Route("/hello", name = "hello_base")
+     * @Route("hello/{prenom}", name ="hello_prenom")
+     * 
+     * Montre la page qui dit bonjour
+     * 
+     * @return void
+     */
+    public function hello($prenom = "anonyme", $age = 0){
+        return $this->render( 'hello.html.twig' , [
+            'prenom' => $prenom,
+            'age' => $age ]
+        );
+    }
+
+    /**
+     * @Route("/", name="homepage")
+     */
+    public function home(){
+
+        $prenoms = ["Lior" => 31 , "Joseph" => 12 , "Anne" => 55];
+
+        return $this->render(
+            'home.html.twig',
+            ['title' => "Bonjour à toi",
+            'age' => 12,
+            'tableau' => $prenoms 
+            ]
+        );
+    }
+
+}
+
+?>
